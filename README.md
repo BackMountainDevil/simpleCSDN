24482539900838ea224dc359b8159b64b96db047
kearney <191615342@qq.com>
2021-10-07 15:14:26 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# CSDN 优雅脚本
+
+## 功能
+
+- 隐藏多余信息，突出文章主体
+- 去广告
+
+## 安装
+
+
+# License
+
+MIT
+
+# 参考
+
+- [极简知乎 - hceasy](https://github.com/hceasy/simpleZhiHu)
+- [CSDNGreener - adlered](https://github.com/adlered/CSDNGreener)
---
+++ simpleCSDN-greasyfork.js
@@ -0,0 +1,40 @@
+// ==UserScript==
+// @name         CSDN 优雅脚本
+// @namespace    https://github.com/BackMountainDevil/
+// @supportURL   https://github.com/BackMountainDevil/simpleCSDN/issues
+// @version      0.0.1
+// @license      MIT
+// @description  油猴脚本- 优雅的浏览 CSDN（去广告、布局调整）
+// @author       小易
+// @match        *://blog.csdn.net/*/article/details/*
+// @grant        none
+// @run-at       document-end
+// ==/UserScript==
+
+function setTitle(_title) {// 修改网页标题名称
+  window.document.title = _title;
+}
+
+
+(function () {
+  'use strict';
+  // setTitle("标题更换测试");
+
+  var cssFix = document.createElement('style');
+  /* 根据 id 或者 类名 隐藏左侧栏目 */
+  cssFix.innerHTML += '#asideProfile{display:none !important;}'; // 博主头像、数据、徽章
+
+  cssFix.innerHTML += '#asideHotArticle{display:none !important;}'; // 热门文章
+  cssFix.innerHTML += '#asideNewComments{display:none !important;}'; // 最新评论
+  cssFix.innerHTML += '#asideCategory{display:none !important;}'; // 分类专栏
+  cssFix.innerHTML += '#asideNewNps{display:none !important;}'; // 向朋友推荐
+  cssFix.innerHTML += '#asideArchive{display:none !important;}'; // 最新文章，此方法隐藏失效
+  cssFix.innerHTML += '#container{display:none !important;}'; //
+  /* 根据 id 或者 类名 隐藏底部栏目 */
+  cssFix.innerHTML += '#copyright-box{display:none !important;}'; // 几乎没人想看的备案号
+  cssFix.innerHTML += '.recommend-box{display:none !important;}'; // 相关文章
+  cssFix.innerHTML += '.template-box{display:none !important;}'; // 皮肤主题显示栏（我又不能调，显摆用？）
+  cssFix.innerHTML += '.left-toolbox{display:none !important;}'; // 没什么用的贴在底部的三连
+
+  document.getElementsByTagName('head')[0].appendChild(cssFix); // 实施隐藏
+})();
